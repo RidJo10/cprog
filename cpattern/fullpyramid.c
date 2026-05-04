13b16e222416b73aa0200f08da84e614a1c51e51
RidJo10 <ridhinsibi2002@gmail.com>
2026-05-04 22:54:32 +0400

    full pyr

--- cpattern/fullpyramid.c
+++ cpattern/fullpyramid.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    for(int i=1;i<5;i++){
-        if(i%2==0)
-        for(int j=0;j<)
+    for(int i=1;i<=5;i++){
+        for(int j=i;j<5;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=(2*i-1);k++){
+            printf("*");
+        }
+        printf("\n");
     }
+    return 0;
 }
