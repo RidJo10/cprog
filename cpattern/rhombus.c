463204926a411314272ecbda0133389f529d43f5
RidJo10 <ridhinsibi2002@gmail.com>
2026-05-21 11:44:12 +0400

    rhombus pattern

---
+++ cpattern/rhombus.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int rhombus(int height);
+
+int main(){
+    int h;
+    printf("Enter height: ");
+    scanf("%d",&h);
+    rhombus(h);
+    return 0;
+}
+
+int rhombus(int a){
+    for(int i=1;i<=a;i++){
+        for(int j=0;j<a-i;j++){
+            printf(" ");
+        }
+        for(int k=0;k<a;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
